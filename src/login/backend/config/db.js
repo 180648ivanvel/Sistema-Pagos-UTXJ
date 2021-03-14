800cf0ea99ec17d87e5e1fe02aba08f297162ed2
Sabdiel <sabdielgutierrez@gmail.com>
2021-03-14 14:00:07 -0600

    Corrección ortográfica de advertencias

--- src/login/backend/config/db.js
+++ src/login/backend/config/db.js
@@ -3,10 +3,10 @@ const dbURL = require('./properties').DB;
 module.exports = () => {
     mogoose.connect(dbURL, { useNewUrlParser: true })
     .then(() => console.log('Mongo conectado en ${dbURL}'))
-    .catch(err => console.log('Erooro en la conexión ${err}'))
+    .catch(err => console.log('Error en la conexión ${err}'))
     process.on('SIGINT', () => {
         mongoose.connection.close(()=>{
-            console.log('Mongo a sido desconectado');
+            console.log('Mongo ha sido desconectado');
             process.exit(0)
         });
     });
